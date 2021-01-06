0fe6cf7b71186c91d3b7afc05ec2c5924d769180
ks289 <ks289@st-andrews.ac.uk>
2021-01-06 14:02:02 +0000

    list items now have dob, and will only display users where dob matches today

--- setup/src/App.js
+++ setup/src/App.js
@@ -2,12 +2,17 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
-  const [people, setPeople] = useState(data)
+  const [people, setPeople] = useState(data);
+  const numeric = { day: 'numeric', month: 'numeric' };
+  const date = new Date();
+  const regex = new RegExp(date.toLocaleDateString('en-GB', numeric), 'g');
+  const filter = people.filter(({ dob }) => dob.match(regex));
+  const len = filter.length;
   return (
     <main>
       <section className='container'>
-        <h3>{people.length} birthdays today</h3>
-        <List people={people}/>
+        <h3>{len} birthdays today</h3>
+        <List people={filter}/>
         <button onClick={() => setPeople([])}>clear all</button>
       </section>
     </main>
--- setup/src/data.js
+++ setup/src/data.js
@@ -3,6 +3,7 @@ export default [
     id: 1,
     name: 'Bertie Yates',
     age: 29,
+    dob: '06/01/1992',
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
   },
@@ -10,6 +11,7 @@ export default [
     id: 2,
     name: 'Hester Hogan',
     age: 32,
+    dob: '06/01/1989',
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
   },
@@ -17,6 +19,7 @@ export default [
     id: 3,
     name: 'Larry Little',
     age: 36,
+    dob: '24/08/1985',
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
   },
@@ -24,6 +27,7 @@ export default [
     id: 4,
     name: 'Sean Walsh',
     age: 34,
+    dob: '01/12/1987',
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
   },
@@ -31,6 +35,7 @@ export default [
     id: 5,
     name: 'Lola Gardner',
     age: 29,
+    dob: '07/01/1992',
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
   },
